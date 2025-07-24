5dddc87bebe91dc2f296cafa1d83c5542ef12ce5
Laiba Siddiqui <laibasiddiqui791@gmail.com>
2025-07-24 09:48:01 +0500

    Create Immediate_Data_Generator.v
    Extracts and sign-extends immediate values from RISC-V instructions based on the instruction format (I, S, B, J).

---
+++ Immediate_Data_Generator.v
@@ -0,0 +1,14 @@
+module imm_data_gen(instruction , ImmSrc , imm_data);
+input [31:0] instruction;
+input [1:0] ImmSrc;
+output reg [31:0] imm_data;
+always @(*) begin
+    case(ImmSrc)
+        2'b00: imm_data = {{20{instruction[31]}}, instruction[31:20]};
+        2'b01: imm_data = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+        2'b10: imm_data = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+        2'b11: imm_data = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+        default: imm_data = 32'b0; 
+    endcase
+end
+endmodule
